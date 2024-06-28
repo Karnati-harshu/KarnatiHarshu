d8dd9fb0e53be8b404c585fd4d1f5fc85251fcef
Harshitha <harshithakarnati2535@gmail.com>
2024-06-28 14:09:31 -0400

    change

--- src/components/MyContent.jsx
+++ src/components/MyContent.jsx
@@ -36,7 +36,7 @@ const MyContent = () => {
               animate="visible"
               className="bg-gradient-to-r from-blue-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
-              Jr. Web Developer
+              Full stack Developer
             </motion.span>
             <motion.p
               variants={container(0.7)}
