7acf2d9396a85cfa538e41c9c6f721363078300e
Harshitha <harshithakarnati2535@gmail.com>
2024-06-28 10:40:10 -0400

    added side nav

--- src/components/MyContent.jsx
+++ src/components/MyContent.jsx
@@ -16,7 +16,7 @@ const container = (delay) => ({
 const MyContent = () => {
   const [menu, setMenu] = useState("menu")
   return (
-    <div id="home" className="border-b border-neutral-900 pb-4 lg:mb-35">
+    <div id="home" className="border-b border-neutral-900 pb-4 h-dvh lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
@@ -24,7 +24,7 @@ const MyContent = () => {
               variants={container(0)}
               initial="hidden"
               animate="visible"
-              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl sm:text-center"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-10 xl:mt-16 xl:text-8xl  lg:xl:pb-8"
             >
               <span className=" font-semibold">H</span>arshitha{" "}
               <span className=" font-semibold">K</span>arnati{" "}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,99 @@
-import React,{ useState, useEffect }  from 'react'
-import logo from '../assets/HKLogo.png'
-import AnchorLink from 'react-anchor-link-smooth-scroll';
+import React, { useState, useEffect } from "react";
+import logo from "../assets/HKLogo.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { CiCircleRemove, CiMenuFries } from "react-icons/ci";
 
 const Navbar = () => {
-  const [menu,setMenu] = useState("menu")
+  const [menu, setMenu] = useState("menu");
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  const closeMenu = () => {
+    setMenuOpen(false);
+  }
   return (
-    <nav className='mb-5 flex items-center justify-between py-6'>
-        <div className='flex flex-shrink-0 items-center'>
-            <img className='mx-2 w-16  text-2xl' src={logo} alt='logo' />
+    <nav className="mb-5 relative flex items-center justify-between py-6 lg:mb-2 lg:py-2">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-16  text-2xl" src={logo} alt="logo" />
+      </div>
+      {/* <div>
+          <button className='block sm:hidden transition'>{click ? <Fatime/> : <CiMenuFries/> }</button>
+        </div> */}
+      <div className="block sm:hidden">
+        <button onClick={toggleMenu} className="transition">
+          <CiMenuFries size={30} />
+        </button>
+      </div>
+      <div
+        className={` fixed top-0 left-0 z-50 h-full w-64 bg-gray-800 transform transition-transform ${menuOpen ? "translate-x-0" : "-translate-x-full"
+          } sm:hidden`}
+      >
+        <div className="flex justify-between items-center p-4">
+          <img className="w-16 text-2xl" src={logo} alt="logo" />
+          <button onClick={toggleMenu} className="text-white sm:hidden">
+            <CiCircleRemove size={30} />
+          </button>
         </div>
-        <div >
-          <ul className=' m-8 flex items-center justify-center gap-6 text-2xl'>
-            <li><AnchorLink className= " no-underline text-white " offset= {50} href='#home' ><p onClick={() => setMenu("home")}>Home</p></AnchorLink></li>
-            <li><AnchorLink className= "no-underline text-white " offset= {50} href='#about' ><p onClick={() => setMenu("about")}>About</p></AnchorLink></li>
-            <li><AnchorLink className= "no-underline text-white " offset= {50} href='#skills' ><p onClick={() => setMenu("skills")}>Skills</p></AnchorLink></li>
-            <li><AnchorLink className= "no-underline text-white " offset= {50} href='#experience' ><p onClick={() => setMenu("experience")}>Experience</p></AnchorLink></li>
-            <li><AnchorLink className= "no-underline text-white " offset= {50} href='#projects' ><p onClick={() => setMenu("projects")}>Projects</p></AnchorLink></li>
-          </ul>
-        </div>
+        <ul className=" m-8 flex flex-col items-center justify-center gap-6 text-2xl sm:mt-0 sm:flex-row">
+          <li>
+            <AnchorLink
+              className=" no-underline text-white "
+              offset={50}
+              href="#home"
+            >
+              <p onClick={closeMenu}>Home</p>
+            </AnchorLink>
+          </li>
+          <li>
+            <AnchorLink
+              className="no-underline text-white "
+              offset={50}
+              href="#about"
+            >
+              <p onClick={closeMenu}>About</p>
+            </AnchorLink>
+          </li>
+          <li>
+            <AnchorLink
+              className="no-underline text-white "
+              offset={50}
+              href="#skills"
+            >
+              <p onClick={closeMenu}>Skills</p>
+            </AnchorLink>
+          </li>
+          <li>
+            <AnchorLink
+              className="no-underline text-white "
+              offset={50}
+              href="#experience"
+            >
+              <p onClick={closeMenu}>Experience</p>
+            </AnchorLink>
+          </li>
+          <li>
+            <AnchorLink
+              className="no-underline text-white "
+              offset={50}
+              href="#projects"
+            >
+              <p onClick={closeMenu}>Projects</p>
+            </AnchorLink>
+          </li>
+        </ul>
+      </div>
+      <div className='hidden sm:flex sm:items-center sm:justify-center'>
+        <ul className='flex items-center gap-6 text-2xl'>
+          <li><AnchorLink className="no-underline text-white" offset={50} href='#home'>Home</AnchorLink></li>
+          <li><AnchorLink className="no-underline text-white" offset={50} href='#about'>About</AnchorLink></li>
+          <li><AnchorLink className="no-underline text-white" offset={50} href='#skills'>Skills</AnchorLink></li>
+          <li><AnchorLink className="no-underline text-white" offset={50} href='#experience'>Experience</AnchorLink></li>
+          <li><AnchorLink className="no-underline text-white" offset={50} href='#projects'>Projects</AnchorLink></li>
+        </ul>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
